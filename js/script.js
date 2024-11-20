98b84a854893a38b5b259deecfdab06da0e56153
Ric Mershon <rmersh@ricmershon.com>
2024-11-20 12:40:06 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const linkInput = document.getElementById('link-input');
+const linkForm = document.getElementById('link-form');
+const errorMessage = document.getElementById('error-message');
+
+function isValidURL(str) {
+    let pattern = new RegExp(
+        '^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+        '((\\d{1,3}\\.){3}\\d{1,3}))' +
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+        '(\\?[;&a-z\\d%_.~+=-]*)?' +
+        '(\\#[-a-z\\d_]*)?$',
+        'i'
+    )
+    return !!pattern.test(str)
+}
+
+linkForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (linkInput.value === '') {
+        errorMessage.innerHTML = "Please enter a link."
+        linkInput.classList.add('border-red');
+    } else if (!isValidURL(linkInput.value)) {
+        errorMessage.innerHTML = "Please enter a valid URL."
+        linkInput.classList.add('border-red');
+    } else {
+        errorMessage.innerHTML = "";
+        linkInput.classList.remove('border-red');
+        alert('Success');
+    }
+});
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Tailwind Shortly
+This is a purely HTML and CSS project intended to demonstrate Tailwind CSS knowledge.
